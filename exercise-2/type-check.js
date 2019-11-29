6517ec955ada51d224ff77a898ccaff3640a1a28
TheoLGG <tlegagneur@myges.fr>
2019-11-29 14:04:52 +0100

    exercise-2 exo1

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -2,5 +2,15 @@ function type_check_v1(value, type) {
     if (value == null && type == null || typeof value == undefined && type == undefined) {
         return true;
     }
+    if (typeof input === type) {
+        if (input === null && type === "object") {
+            return false
+        }
+        return true
+    }
+    if (type === "array" && input.constructor === Array ) {
+        return true
+    }
 
+    return false
 }
